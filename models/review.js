473e06d991fcdc851bb1cb8ca18fdc6521215fa3
Chris Hutchinson <chrisbond19@me.com>
2018-04-30 20:54:13 -0500

    Changed favicon and title

--- models/review.js
+++ models/review.js
@@ -27,6 +27,20 @@ const reviewSchema = new Schema({
   updated_at  : {type: Date}
 });
 
+reviewSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
 const Review = mongoose.model("Review", reviewSchema);
 
 module.exports = Review;
